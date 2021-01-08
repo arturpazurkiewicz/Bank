69005d76a39b04531ae300643cd83422f3f4a2ad
Artur Pazurkiewicz <pazurkiewiczartur@gmail.com>
2021-01-08 13:31:57 +0100

    fix

--- bank/settings.py
+++ bank/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'www_bank'
+    'www_bank',
+    'argon2'
 ]
 
 MIDDLEWARE = [
